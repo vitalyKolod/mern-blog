6a758ce445d1663521ac9d47e380f8ccc47e0952
Vitaly Kolodchenko <kolodchenkovitaly250@gmail.com>
2025-09-10 14:49:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('1111 Hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+	console.log(req.body)
+
+	const token = jwt.sign(
+		{
+			email: req.body.email,
+			fullName: 'Виталий Колодченко',
+		},
+		'secret123'
+	)
+	res.json({
+		success: true,
+		token,
+	})
+})
+
+app.listen(4444, err => {
+	if (err) {
+		return console.log(err)
+	}
+	console.log('Server OK')
+})
