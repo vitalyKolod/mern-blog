14d92d2dd99a4659c76e9521e87267e02ee551dc
Vitaly Kolodchenko <kolodchenkovitaly250@gmail.com>
2025-09-11 16:51:27 +0300

    db adn user create

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 import { registerValidation } from './validations/auth.js'
 import { validationResult } from 'express-validator'
+import UserModel from './models/User.js'
 
 mongoose
 	.connect(
-		'mongodb+srv://kolodchenkoVitaly:kolodchenkoVit19@cluster0.hqh18zh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+		'mongodb+srv://kolodchenkoVitaly:kolodchenkoVit19@cluster0.hqh18zh.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0'
 	)
 	.then(() => {
 		console.log('db ok')
@@ -19,15 +21,30 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
 	const errors = validationResult(req)
 	if (!errors.isEmpty()) {
 		return res.status(400).json(errors.array())
 	}
 
-	res.json({
-		success: true,
+	// Шифруем пароль
+	const password = req.body.password
+	const salt = await bcrypt.genSalt(10)
+	const passwordHash = await bcrypt.hash(password, salt)
+
+	// Создаем пользователя
+	const doc = new UserModel({
+		email: req.body.email,
+		fullName: req.body.fullName,
+		avatarUrl: req.body.avatarUrl,
+		email: req.body.email,
+		passwordHash: req.body.email,
+		passwordHash,
 	})
+
+	const user = await doc.save()
+
+	res.json(user)
 })
 
 app.listen(4444, err => {
